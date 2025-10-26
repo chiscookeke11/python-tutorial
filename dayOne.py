10ceaf7640b2c1f2636a96346e25ee03151b611f
chiscookeke11 <chiscookeke11@gmail.com>
2025-10-26 07:25:09 +0100

    puash

---
+++ dayOne.py
@@ -0,0 +1,29 @@
+site_name = 'chinedu'
+
+
+print(site_name)
+
+a, b, c = 1, 2, 3
+
+
+print(a)
+print(b)
+print(c)
+
+
+
+# python data types
+# 1. integer = int, float, complex
+# 2. string = str
+# 3. sequence = list, tuple, range
+# 4. mapping = dict
+# 5. boolean = bool
+# 6. set = set
+
+
+num1 = 55
+num2 = 55.5
+
+
+print(type(num1))
+print(type(num2))
