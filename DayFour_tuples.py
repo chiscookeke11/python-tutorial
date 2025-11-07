fbaf91485b3f0478cf3db3bcd14a8673e0d4b8bc
chiscookeke11 <chiscookeke11@gmail.com>
2025-11-07 07:23:42 +0100

    updates

---
+++ DayFour_tuples.py
@@ -0,0 +1,13 @@
+# The two ways a tuple can be written
+var1 = ("hello", "gtr", "rerer", "brown", 99)
+var2 = "hello",
+
+print(type(var1))
+
+
+
+# Accessing tuple elements through indexing
+print(var1[3])
+
+# Accessing tuple elemnts using negative indexing
+print(var1[-1])
