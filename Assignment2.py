066c2f94aa09b9a3b75ea4bfec98d696e6a7fb96
chiscookeke11 <chiscookeke11@gmail.com>
2025-12-24 20:08:30 +0100

    week 2 assignment

---
+++ Assignment2.py
@@ -0,0 +1,26 @@
+my_list = []
+
+
+# appending items to the list
+my_list = [10, 20, 30, 40]
+
+# insert the value 15 at the second position
+my_list.insert(2, 15)
+
+
+# extending the list
+my_list.extend([50, 60, 70])
+
+
+# removing the last element from the list
+my_list.pop()
+
+
+
+# sorting the list
+my_list.sort()
+
+
+# print(sorted_list)
+print(my_list)
+print(my_list.index(30))
