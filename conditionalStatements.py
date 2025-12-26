943349ed60fd5bd8adfbefe58721f777f4e91e9e
chiscookeke11 <chiscookeke11@gmail.com>
2025-12-26 05:16:27 +0100

    conditional statements lesson

---
+++ conditionalStatements.py
@@ -0,0 +1,23 @@
+temperature = 30
+print("Program started")
+
+if temperature < 25:
+    print("It's a cold day!")
+
+elif temperature == 30:
+    print("The temperature is 30 deg")
+
+
+else: print("Today is hot ")
+
+
+
+# Example of an if...elif...else statement
+temperature = 15
+
+if temperature > 25:
+    print("It's a hot day!")
+elif temperature > 15:
+    print("It's a warm day!")
+else:
+    print("It's a cold day!")
